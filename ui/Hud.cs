3f1413a6da98fa5bae72edbb47635dd34c96c0fa
Grendel <jcg@grendel.world>
2023-02-15 21:35:38 -0600

    Add crosshair

--- ui/Hud.cs
+++ ui/Hud.cs
@@ -4,6 +4,7 @@ using GodotOnReady.Attributes;
 public partial class Hud : Control
 {
     [OnReadyGet("%GrabbyHand")] private TextureRect _hand;
+    [OnReadyGet("%Crosshair")] private TextureRect _crosshair;
 
     public void SetCashMoney(int amount)
     {
@@ -13,10 +14,12 @@ public partial class Hud : Control
     public void ShowHand()
     {
         _hand.Visible = true;
+        _crosshair.Visible = false;
     }
 
     public void HideHand()
     {
         _hand.Visible = false;
+        _crosshair.Visible = true;
     }
 }
