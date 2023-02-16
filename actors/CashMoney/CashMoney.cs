35e383d5b34e49812a73a2c54f432f3927125f53
Grendel <jcg@grendel.world>
2023-02-15 21:34:40 -0600

    Use AnimationPlayer to spin

--- actors/CashMoney/CashMoney.cs
+++ actors/CashMoney/CashMoney.cs
@@ -2,13 +2,7 @@ using Godot;
 
 public partial class CashMoney : Area, IInteractable
 {
-    [Export] private readonly float _spinRate = Mathf.Pi;
     [Export] private readonly int _value = 1;
-
-    public override void _Process(float delta)
-    {
-        RotateY(_spinRate * delta);
-    }
 
     public void Interact(Node byWhom)
     {
