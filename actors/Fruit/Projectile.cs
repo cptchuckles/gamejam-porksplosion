d534050f51ea4345b91d3c043fa1abc7664d5b74
Grendel <jcg@grendel.world>
2023-02-16 20:09:04 -0600

    Add Fruit (apple) scene and behavior

---
+++ actors/Fruit/Projectile.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public class Projectile : RigidBody
+{
+    [Export] private float _launchForce = 10f;
+    [Export] private readonly float _launchTorque = 3f;
+    [Export] private readonly float _lifeSpan = 5f;
+
+    public override void _Ready()
+    {
+        if (GetParent().Owner is IFirearm firearm)
+        {
+            _launchForce = Mathf.Max(_launchForce, firearm.LaunchForce);
+        }
+        SetAsToplevel(true);
+
+        ApplyCentralImpulse(-GlobalTransform.basis.z * _launchForce);
+
+        Vector3 vector3Random = new Vector3(GD.Randf(), GD.Randf(), GD.Randf()).Normalized();
+        ApplyTorqueImpulse(vector3Random * GD.Randf() * _launchTorque);
+
+        GetTree().CreateTimer(_lifeSpan).Connect("timeout", this, "queue_free");
+    }
+}
